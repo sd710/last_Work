b5c3b5a85af07eaf8123c0a7a56bddd3796fdbbd
Dmitriy Subbotin <subbotin.dmitriy94@gmail.com>
2016-05-16 10:16:53 +0300

    прошёл 2 урока

---
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Concrete;
+
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // Здесь размещаются привязки
+            //Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            //mock.Setup(m => m.Games).Returns(new List<Game>
+            //{
+            //    new Game { Name = "FIFA 16", Price = 1999 },
+            //    new Game { Name = "FIFA 15", Price = 999 },
+            //    new Game { Name = "Uncharted 4", Price = 3990 }
+            //});
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+        }
+    }
+}
