7ff36414b0ed307e21823560dab6c7e35daaf638
Dmitriy Subbotin <subbotin.dmitriy94@gmail.com>
2016-06-07 14:30:33 +0300

    Следующий шаг, оплата через платежные системы

--- GameStore.WebUI/Controllers/CartController.cs
+++ GameStore.WebUI/Controllers/CartController.cs
@@ -52,5 +52,10 @@ namespace GameStore.WebUI.Controllers
         {
             return PartialView(cart);
         }
+
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
